0cfe79f8ed13a275fe4077086f1513043740c8ec
michael-stac <mickymouze778@gmail.com>
2023-11-20 02:09:43 +0100

    added appcolor and shared service

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:bubbl/splash_screen.dart';
+import 'package:bubbl/screens/splash_screen.dart';
+import 'package:bubbl/utils/appcolors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +13,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          fontFamily: 'Inter',
+          appBarTheme: const AppBarTheme(
+              systemOverlayStyle: SystemUiOverlayStyle.light,
+              backgroundColor: Colors.white, elevation: 0),
+          highlightColor: AppColor.grayColor,
+          primaryColor: AppColor.primaryMaterialColor,
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: AppColor.primaryMaterialColor)
+              .copyWith(secondary: AppColor.primaryMaterialColor)),
 
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
