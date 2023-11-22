2092f3d231709df0193696949f26da6ffdf988b4
michael-stac <mickymouze778@gmail.com>
2023-11-22 13:56:15 +0100

    completed user home screen

--- lib/screens/bottom_nav_pages/home/home.dart
+++ lib/screens/bottom_nav_pages/home/home.dart
@@ -83,181 +83,182 @@ class _HomeScreenState extends State<HomeScreen> {
                   decoration: const BoxDecoration(color: Colors.white),
                   padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
                   margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                  child: Center(
-                    child: Container(
-                      constraints: const BoxConstraints(maxWidth: 400),
-                      decoration: BoxDecoration(
-                        color: AppColor.whiteSmokeColor2,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: 150,
-                            child: Stack(
-                              children: [
-                                ClipPath(
-                                  clipper: AvatarClipper(),
-                                  child: Container(
-                                    height: 100,
-                                    decoration: BoxDecoration(
-                                      color: AppColor.primaryColor,
-                                      borderRadius: const BorderRadius.only(
-                                        topRight: Radius.circular(10),
-                                        topLeft: Radius.circular(10),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Positioned(
-                                  left: 11,
-                                  top: 50,
-                                  child: Row(
-                                    children: [
-                                      const CircleAvatar(
-                                        radius: 50,
-                                        backgroundImage: NetworkImage(
-                                            "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/22/22a4f44d8c8f1451f0eaa765e80b698bab8dd826_full.jpg"),
-                                      ),
-                                      const SizedBox(width: 20),
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            "Max Weber",
-                                            style: TextStyle(
-                                              fontSize: 20,
-                                              fontFamily: 'Inter',
-                                              color: AppColor.white,
-                                              fontWeight: FontWeight.w700,
-                                            ),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: ListView.builder(
+                      itemCount: 13,
+                      itemBuilder: (context, index) {
+
+                      return Center(
+                        child: Container(
+                          margin: EdgeInsets.only(bottom: 20),
+                          constraints: const BoxConstraints(maxWidth: 400),
+                          decoration: BoxDecoration(
+                            color: AppColor.whiteSmokeColor2,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 150,
+                                child: Stack(
+                                  children: [
+                                    ClipPath(
+                                      clipper: AvatarClipper(),
+                                      child: Container(
+                                        height: 100,
+                                        decoration: BoxDecoration(
+                                          color: AppColor.primaryColor,
+                                          borderRadius: const BorderRadius.only(
+                                            topRight: Radius.circular(10),
+                                            topLeft: Radius.circular(10),
                                           ),
-                                          const SizedBox(height: 8),
-                                          RichText(
-                                              text: TextSpan(children: [
-                                            TextSpan(
-                                                text: 'Flutter Developer',
+                                        ),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      left: 11,
+                                      top: 50,
+                                      child: Row(
+                                        children: [
+                                          const CircleAvatar(
+                                            radius: 50,
+                                            backgroundImage: NetworkImage(
+                                                "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/22/22a4f44d8c8f1451f0eaa765e80b698bab8dd826_full.jpg"),
+                                          ),
+                                          const SizedBox(width: 20),
+                                          Column(
+                                            crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "Max Weber",
                                                 style: TextStyle(
-                                                    fontSize: 16,
-                                                    fontWeight: FontWeight.w700,
-                                                    fontFamily: 'Inter',
-                                                    color: AppColor.black))
-                                          ])),
-                                          const SizedBox(height: 8),
+                                                  fontSize: 20,
+                                                  fontFamily: 'Inter',
+                                                  color: AppColor.white,
+                                                  fontWeight: FontWeight.w700,
+                                                ),
+                                              ),
+                                              const SizedBox(height: 8),
+                                              RichText(
+                                                  text: TextSpan(children: [
+                                                    TextSpan(
+                                                        text: 'Flutter Developer',
+                                                        style: TextStyle(
+                                                            fontSize: 16,
+                                                            fontWeight: FontWeight.w700,
+                                                            fontFamily: 'Inter',
+                                                            color: AppColor.black))
+                                                  ])),
+                                              const SizedBox(height: 8),
+                                            ],
+                                          )
                                         ],
-                                      )
-                                    ],
-                                  ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 10,
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Mentoring Style",
+                                      style: TextStyle(
+                                          color: AppColor.black,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    const ReadMoreText(
+                                      'I tailor mentoring to your unique learning needs, blending structured guidance with flexibility. Whether you prefer a curriculum, hands-on projects, or open discussions, I adapt to your style. Lets collaborate to explore concepts, tackle challenges, and build a roadmap for your tech industry growth, making your learning journey effective and enjoyable',
+                                      trimLines: 3,
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 13,
+                                          height: 2),
+                                      colorClickableText: Color(0xff006400),
+                                      trimMode: TrimMode.Line,
+                                      trimCollapsedText: 'read more',
+                                      trimExpandedText: ' see less ',
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              PageService.textSpacexxL,
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 8.0, right: 8.0, bottom: 8.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "6280",
+                                          style: PageService.textEditStyle,
+                                        ),
+                                        Text(
+                                          "Mentees",
+                                          style: PageService.labelStyle,
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 50,
+                                      child: VerticalDivider(
+                                        color: AppColor.black,
+                                      ),
+                                    ),
+
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "1745",
+                                          style: PageService.textEditStyle,
+                                        ),
+                                        Text(
+                                          "Courses",
+                                          style: PageService.labelStyle,
+                                        )
+                                      ],
+                                    ),
+                                     SizedBox(
+                                      height: 50,
+                                      child: VerticalDivider(
+                                        color: AppColor.black,
+                                      ),
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "163",
+                                          style: PageService.textEditStyle,
+                                        ),
+                                        Text(
+                                          "Videos",
+                                          style: PageService.labelStyle,
+
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(height: 8)
+                            ],
                           ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 16,
-                              vertical: 10,
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Mentoring Style",
-                                  style: TextStyle(
-                                      color: AppColor.black,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                                const ReadMoreText(
-                                  'I tailor mentoring to your unique learning needs, blending structured guidance with flexibility. Whether you prefer a curriculum, hands-on projects, or open discussions, I adapt to your style. Lets collaborate to explore concepts, tackle challenges, and build a roadmap for your tech industry growth, making your learning journey effective and enjoyable',
-                                  trimLines: 3,
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 13,
-                                      height: 2),
-                                  colorClickableText: Color(0xff006400),
-                                  trimMode: TrimMode.Line,
-                                  trimCollapsedText: 'read more',
-                                  trimExpandedText: ' see less ',
-                                ),
-                              ],
-                            ),
-                          ),
-                          PageService.textSpacexxL,
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                left: 8.0, right: 8.0, bottom: 8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                const Column(
-                                  children: [
-                                    Text(
-                                      "6280",
-                                    ),
-                                    Text(
-                                      "Mentees",
-                                    )
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 50,
-                                  child: VerticalDivider(
-                                    color: AppColor.black,
-                                  ),
-                                ),
-                                const Column(
-                                  children: [
-                                    Text(
-                                      "6280",
-                                    ),
-                                    Text(
-                                      "Mentees",
-                                    )
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 50,
-                                  child: VerticalDivider(
-                                    color: AppColor.black,
-                                  ),
-                                ),
-                                const Column(
-                                  children: [
-                                    Text(
-                                      "1745",
-                                    ),
-                                    Text(
-                                      "Courses",
-                                    )
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 50,
-                                  child: VerticalDivider(
-                                    color: Color(0xFF9A9A9A),
-                                  ),
-                                ),
-                                const Column(
-                                  children: [
-                                    Text(
-                                      "163",
-                                    ),
-                                    Text(
-                                      "Videos",
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 8)
-                        ],
-                      ),
-                    ),
+                        ),
+                      );
+                    },),
                   ),
                 ),
               ),
